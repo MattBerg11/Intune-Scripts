0205d0a0f4db61c29276ed254ad8a1f393b89f0d
Matt Berg <25282996+MattBerg11@users.noreply.github.com>
2024-01-30 15:20:30 -0600

    Create Unpin Windows Store-Rem.ps1

---
+++ Remediations/Unpin Windows Store-Rem.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Append -Path "C:\IntuneFiles\Logs\UnpinWindows.log" -Force
+
+$apps = ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items())
+foreach ($app in $apps) {
+    $appname = $app.Name
+    if ($appname -like "*store*") {
+        $finalname = $app.Name
+    }
+}
+
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ? { $_.Name -eq $finalname }).Verbs() | ? { $_.Name.replace('&', '') -match 'Unpin from taskbar' } | % { $_.DoIt(); $exec = $true }
+
+Stop-Transcript
